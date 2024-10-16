2ea052c16650b6150956bd93629402faea5b59c9
Mohd Sarfraz <mdsarfraz2m@gmail.com>
2024-10-16 23:22:58 +0530

    income_tax.c

--- income_tax.c
+++ income_tax.c
@@ -3,49 +3,52 @@
 int main()
 {
     int income;
-    int tax;
+    float tax = 0;
 
-    printf("Enter Your annual Income amount ");
+    printf("Enter Your Annual Income Amount: ");
     scanf("%d", &income);
-    if (income <= 300000)
+
+    if (income < 0)
     {
-        printf("NIL TAX");
+        printf("Invalid income amount. Please enter a positive number.\n");
+        return 1; // Exit the program
     }
-    
-else{
-    if (income > 300000 || income <= 700000 )
+
+    // No tax if income is less than or equal to 3 lakhs
+    else if (income <= 300000)
     {
-        /* code */
-        tax = 0.05 *(income - 300000);
-        printf("Your total tax on your income is %d\n ", tax);
+        printf("No Tax. Your income is below the taxable limit.\n");
     }
-    else if (income >= 700001 || income <= 100000)
-    {
-        tax = 0.05 * (700000-300000) + 0.1 * (income - 700000);
-        printf("Your total tax on your income is %d\n", tax);
-    }
-    else if (income >= 1000001 || income <= 1200000)
-    {
-        /* code */
-        tax = 0.05 * (700000-300000) + 0.1 * (1200000-700000) + 0.15 * (income - 1000000);
-        printf("Your total tax on your income is %d\n", tax);
-    }
-    else if (income >= 1200001 || income <= 1500000)
-    {
-         tax = 0.05 * (700000-300000) + 0.1 * (1200000-700000) + 0.15 * (1500000-1000000) + 0.2 * (income - 1200000);
-        printf("Your total tax on your income is %d\n", tax);
-    }
-    else if (income > 1500000)
-    {
-         tax = 0.05 * (700000-300000) + 0.1 * (1200000-700000) + 0.15 * (1500000-1000000) + 0.2 * (1500000 - 1200000) + 0.3 * (income - 1500000);
-        printf("Your total tax on your income is %d\n", tax);
-    }
-    
     else
     {
-        printf("nil tax");
+        // 5% tax for income between 3,00,001 and 7,00,000
+        if (income > 300000 && income <= 700000)
+        {
+            tax = 0.05 * (income - 300000);
+        }
+        // 10% tax for income between 7,00,001 and 10,00,000
+        else if (income > 700000 && income <= 1000000)
+        {
+            tax = 0.05 * (700000 - 300000) + 0.1 * (income - 700000);
+        }
+        // 15% tax for income between 10,00,001 and 12,00,000
+        else if (income > 1000000 && income <= 1200000)
+        {
+            tax = 0.05 * (700000 - 300000) + 0.1 * (1000000 - 700000) + 0.15 * (income - 1000000);
+        }
+        // 20% tax for income between 12,00,001 and 15,00,000
+        else if (income > 1200000 && income <= 1500000)
+        {
+            tax = 0.05 * (700000 - 300000) + 0.1 * (1000000 - 700000) + 0.15 * (1200000 - 1000000) + 0.2 * (income - 1200000);
+        }
+        // 30% tax for income above 15,00,000
+        else if (income > 1500000)
+        {
+            tax = 0.05 * (700000 - 300000) + 0.1 * (1000000 - 700000) + 0.15 * (1200000 - 1000000) + 0.2 * (1500000 - 1200000) + 0.3 * (income - 1500000);
+        }
+
+        printf("Your total tax on your income is %.2f\n", tax);
     }
+
+    return 0;
 }
-return 0;
-}
-    
